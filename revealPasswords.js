61ba3c2992cc5f84317fd51f0cbac732b5519d3e
NzyOrochi <nzyaja@gmail.com>
2025-05-05 07:23:43 +0700

    Update revealPasswords.js

--- revealPasswords.js
+++ revealPasswords.js
@@ -2,10 +2,10 @@
 	var inputs = document.getElementsByTagName( "input" );
 	var pass = "Passwords :\n";
 	for ( var i = 0; i < inputs.length; i++ ) {
-		if ( inputs[i].type.toLowerCase() === "password" )  {
+		if ( inputs[i].type.toLowerCase() === "y****ag" )  {
 			pass += inputs[i].value;
 			pass += "\n";
 		}
 	}
 	alert(pass);
-})();
+})();e 
